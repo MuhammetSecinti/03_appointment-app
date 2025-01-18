a2ba54940d3da01a895cb4598893a38a03640ecf
Muhammet <secintimuhammet@gmail.com>
2025-01-19 00:38:58 +0300

    form-state-added

--- src/components/AddModal.jsx
+++ src/components/AddModal.jsx
@@ -1,30 +1,47 @@
-import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
+import { useState } from "react";
+import { Form } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 
-function AddModal({showModal , handleClose}) {
-  
-
- 
-  
+function AddModal({ showModal, handleClose, drName }) {
+  const [patientName, setPatienName] = useState("");
+  const [date, setDate] = useState("");
 
   return (
     <>
-      
-
       <Modal show={showModal} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>Appointment for: {drName}</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Patient Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter Patient Name"
+                onChange={(e) => setPatienName(e.target.value)}
+                value={patientName}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Date</Form.Label>
+              <Form.Control
+                type="datetime-local"
+                onChange={(e) => setDate(e.target.value)}
+              />
+            </Form.Group>
+          </Form>
+          <div className="text-center">
+            <Button variant="success" type="submit" className="m-2">
+              Save
+            </Button>
+            <Button variant="danger" onClick={handleClose}>
+              Close
+            </Button>
+          </div>
+        </Modal.Body>
       </Modal>
     </>
   );
--- src/components/Doctors.jsx
+++ src/components/Doctors.jsx
@@ -1,39 +1,46 @@
-import Container from "react-bootstrap/Container"
-import {doctorData} from "../helper/data"
-import { Col, Image, Row } from "react-bootstrap"
+import Container from "react-bootstrap/Container";
+import { doctorData } from "../helper/data";
+import { Col, Image, Row } from "react-bootstrap";
 
 import AddModal from "./AddModal";
 import { useState } from "react";
 
-
-
 const Doctors = () => {
   const [showModal, setShow] = useState(false);
+  const [drName, setDrName] = useState("");
 
-  const handleShow = () => setShow(true);
   const handleClose = () => setShow(false);
 
-  console.log(doctorData);
+  
   return (
     <Container className="p-2">
       <h3 className="display-6 mb-3" style={{ color: "rgb(166, 18, 189)" }}>
         Our Doctors
       </h3>
       <Row>
-        {
-          doctorData.map(doctor => 
-            <Col xs={6} sm={4} lg={3} key={doctor.id}>
-            <Image onClick={handleShow} className="img-thumbnail doctor-img" src={doctor.img} alt={doctor.name} />
+        {doctorData.map((doctor) => (
+          <Col xs={6} sm={4} lg={3} key={doctor.id}>
+            <Image
+              onClick={() => {
+                setShow(true);
+                setDrName(doctor.name);
+              }}
+              className="img-thumbnail doctor-img"
+              src={doctor.img}
+              alt={doctor.name}
+            />
             <h5>{doctor.name}</h5>
             <p>{doctor.dep}</p>
-            </Col>
-          )
-        }
+          </Col>
+        ))}
       </Row>
-      <AddModal showModal = {showModal} handleClose = {handleClose} />
-
+      <AddModal
+        showModal={showModal}
+        handleClose={handleClose}
+        drName={drName}
+      />
     </Container>
-  )
-}
+  );
+};
 
-export default Doctors
+export default Doctors;
